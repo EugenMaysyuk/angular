e9839facc86064b7dedb2a87063c451e15f1aaea
Eugen Maysyuk <eugenmaysyuk@coherentsolutions.com>
2015-08-27 16:38:09 +0300

    Task 1

---
+++ js/app.js
@@ -0,0 +1,12 @@
+(function() {
+	var app = angular.module('task1', []);
+
+	app.controller('parentCtrl1', function() {});
+
+	app.controller('parentCtrl2', function() {});
+
+	app.controller('childCtrl1', function() {});
+
+	app.controller('childCtrl2', function() {});
+
+})();
